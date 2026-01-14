f3a1d17533e9c314a9c4205cef61e3582a720493
Roni Biswas <ronibiswas068@gmail.com>
2026-01-14 15:24:10 +0600

    Path extension, path join, path resolve, path normalize

--- 4. path-module/index.js
+++ 4. path-module/index.js
@@ -2,3 +2,13 @@ const path = require("path");
 
 console.log("Directory Name: ", path.dirname(__filename));
 console.log("File Name: ", path.basename(__filename));
+console.log("File extension Name: ", path.extname(__filename));
+
+const joinPath = path.join("/user", "documents", "node", "project");
+console.log("Join Path: ", joinPath);
+
+const resolvePath = path.resolve("user", "documents", "node", "projects");
+console.log("Resolve Path: ", resolvePath);
+
+const normalizePath = path.normalize("/user/.documents/../projects");
+console.log("Normalize Path: ", normalizePath);
