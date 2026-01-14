a8680c125b5cc0e42af8d437d3b5021d3f233dde
Roni Biswas <ronibiswas068@gmail.com>
2026-01-14 12:46:46 +0600

    First console hello world using nodejs in command prompt

---
+++ 1. hello-world-node/index.js
@@ -0,0 +1,11 @@
+console.log("Hello, World!");
+
+const array = [1, 2, 3, 4];
+
+console.log(array, "array");
+
+setTimeout(() => {
+  console.log("This message is delayed by 2 second.");
+}, 2000);
+
+console.log("This is last line of the sync code.");
