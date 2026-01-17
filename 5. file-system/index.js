887f290093f2ab15d9b820aa91d192a152c3ac30
Roni Biswas <ronibiswas068@gmail.com>
2026-01-17 13:12:31 +0600

    added fs-(file system) for file create, write content, read content and append content using async way

--- 5. file-system/index.js
+++ 5. file-system/index.js
@@ -17,9 +17,30 @@ fs.writeFileSync(filePath, "Hello, World!");
 
 // sync way to read file
 const readContentFromFile = fs.readFileSync(filePath, "utf-8");
-console.log("Read Content: ", readContentFromFile);
+// console.log("Read Content: ", readContentFromFile);
 
 fs.appendFileSync(filePath, "\nThis is new line added using appendFileSync");
 
 const appendNewContent = fs.readFileSync(filePath, "utf-8");
-console.log("2nd Time read: \n", appendNewContent);
+// console.log("2nd Time read: \n", appendNewContent);
+
+// async way to creating file
+
+const asyncFilePath = path.join(dataFolder, "async-example.txt");
+
+fs.writeFile(asyncFilePath, "Hello, Async node js.", (err) => {
+  if (err) throw err;
+  console.log("Async file is created successfully.");
+  fs.readFile(asyncFilePath, "utf-8", (err, data) => {
+    if (err) throw err;
+    console.log("Read Content by async way: ", data);
+    fs.appendFile(asyncFilePath, "\nThis new line is added!", (err) => {
+      if (err) throw err;
+      console.log("New line has been added!");
+      fs.readFile(asyncFilePath, "utf-8", (err, updatedContent) => {
+        if (err) throw err;
+        console.log("New Updated Content: ", updatedContent);
+      });
+    });
+  });
+});
