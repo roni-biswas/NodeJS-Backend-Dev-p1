aa0deb993847d094d8732f66ef8a9eb4ce0b8526
Roni Biswas <ronibiswas068@gmail.com>
2026-01-19 22:53:54 +0600

    Implement pkg ejs and express, and also set view engine & set views folder path

---
+++ 12. EJS/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// set the view engine as ejs
+app.set("view engine", "ejs");
+
+// set the directory for the views
+app.set("views", path.join(__dirname, "views"));
